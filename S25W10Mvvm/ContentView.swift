dac6eeaf267996049bbeff8b2b479cb5bde313df
백성열 <paek678@naver.com>
2025-11-09 23:00:40 -0800

    Initial Commit

---
+++ S25W10Mvvm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  S25W10Mvvm
+//
+//  Created by Student on 11/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ S25W10Mvvm/S25W10MvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  S25W10MvvmApp.swift
+//  S25W10Mvvm
+//
+//  Created by Student on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct S25W10MvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
